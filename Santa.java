39d858255d7f5448a1592f2bc80a642ae2ed9029
Metin-Trampov <63451415+MetinTrampov@users.noreply.github.com>
2022-06-11 06:07:24 +0300

    Add files via upload

---
+++ Santa.java
@@ -0,0 +1,32 @@
+package Singleton;
+
+public class Santa
+{
+
+  private static Santa instance ;
+    // private static Object MagicDesc;
+    String basicSentences ;
+  String hasCommand (String command) {
+
+        return command;
+    }
+
+    private Santa()
+        {
+                // TODO Auto-generated constructor stub
+        }
+        public static Santa getInstance()
+
+        {
+            if(instance==null){
+                instance =  new Santa();
+
+            }
+            return  instance;
+        }
+
+    public  void postMessage(String message) {
+        System.out.println("Expected object : "+ message);
+        System.out.printf(" Santa said  :  ");
+    }
+}
